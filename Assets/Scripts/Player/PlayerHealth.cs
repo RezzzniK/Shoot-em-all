b1f00deb0d76ffca8ee8f9ae3312d0a7331058f6
Alexander Reznik <alexander.reznik@play.works>
2025-05-17 17:52:50 +0300

    Death Camera Transition

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,21 +1,28 @@
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int playerHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathCamera;
+    [SerializeField] Transform weaponCamera;//we need only transform from this camera to make transition to death camera
+    [SerializeField] int deathCameraPriority = 20;
      public void TakeDamage(int damageAmount)
     {
-       
         playerHealth -= damageAmount;
-
-
         if (playerHealth <= 0)
         {
-          Destroy(this.gameObject);
+            //before we destroy our player GameObj lets unparent our weapon camera:
+            weaponCamera.parent = null;
+            //before we transfer our weapon camera to another virtual camera, if post-processing was toggled on
+            //we will need to toggle on post-procsessing on new camera
+
+            //also we need to change priority on new virtual camera that we will create later (weaponCamera has priority 10)
+            deathCamera.Priority =deathCameraPriority ;
+            Destroy(this.gameObject);
 
         }
 
     }
-
    
 }
