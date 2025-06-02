6a3923aa31c9768ddfc56dee16a3f8d772b732bb
Alexander Reznik <alexander.reznik@play.works>
2025-06-02 21:57:02 +0300

    Win condition and enemies counter

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,7 +6,14 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] int hitPoints = 10;
     [SerializeField] ParticleSystem[] hitExp;
     [SerializeField] ParticleSystem robotExp;
+    GameManager manager;
+
+    void Start()
+    {
     
+        manager = FindFirstObjectByType<GameManager>();
+        manager.updateEnemiesQuant(1);
+    }
     public void TakeDamage(RaycastHit hit, int damageAmount)
     {
         //TODO transform on the particle sys where is the hit point
@@ -39,6 +46,7 @@ public class EnemyHealth : MonoBehaviour
     public void SelfDestruct()
     {
         Instantiate(robotExp, transform.position, Quaternion.identity);
+        manager.updateEnemiesQuant(-1);
         Destroy(this.gameObject);
     }
 
--- Assets/Scripts/Other/GameManager.cs
+++ Assets/Scripts/Other/GameManager.cs
@@ -1,11 +1,27 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] TMP_Text enemies_counter_text;
+    [SerializeField] GameObject you_win_txt;
+    string text="Remain to Eliminate: ";
+    int enemiesLeft=0;
+    public void updateEnemiesQuant(int quant)
+    {
+        enemiesLeft+=quant;
+        enemies_counter_text.text = text +enemiesLeft;
+        if (enemiesLeft <= 0)
+        {
+            you_win_txt.SetActive(true);
+            Invoke("Restart", 1f);
+        }
+    }
     public void Restart()
     {
-        
+
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         Debug.Log("Restart");
     }
